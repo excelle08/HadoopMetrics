e9e00b7ec4755e0f8c615a8ced403bc141c68b28
Excelle Su <suwei779@126.com>
2016-11-11 20:40:01 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,24 @@
+config = {
+    "master": "brix-40",
+    "slaves": [
+        "brix-03",
+        "brix-04",
+        "brix-05",
+        "brix-06",
+        "brix-07",
+        "brix-08",
+        "brix-09",
+        "brix-41",
+        "brix-42"
+    ],
+    "node_port": 8080,
+    "port": 5000,
+    "interval": 60,
+    "database": {
+        "user": "root",
+        "password": "",
+        "host": "127.0.0.1",
+        "port": "3306",
+        "dbname": "hadoop_metrics"
+    }
+}
